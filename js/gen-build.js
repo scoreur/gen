a235ef9852162b5ea57153153a9e2174ef94c5bd
apwan06 <471369331@qq.com>
2016-07-09 15:04:25 -0400

    update generated files

--- js/gen-build.js
+++ js/gen-build.js
@@ -678,7 +678,14 @@
 }).call(this);
 
 //# sourceMappingURL=ScoreObj.js.map
-;
+;function dataURLtoBlob(dataurl) {
+	var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+			bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+	while(n--){
+		u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new Blob([u8arr], {type:mime});
+}
 var seqPlayer = {
 	channel:0,
 	enabled:false,
