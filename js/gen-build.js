c853d66b4d8551da7467fb4e6757613d551c978e
apwan06 <471369331@qq.com>
2016-08-16 21:34:32 -0400

    add generated file

--- js/gen-build.js
+++ js/gen-build.js
@@ -24,6 +24,70 @@
     'Sound effects': ['120 Reverse Cymbal', '121 Guitar Fret Noise', '122 Breath Noise', '123 Seashore', '124 Bird Tweet', '125 Telephone Ring', '126 Helicopter', '127 Applause', '128 Gunshot']
   };
 
+  MG.percussion = {
+    27: "High-Q",
+    28: "Slap",
+    29: "Scratch Push",
+    30: "Scratch Pull",
+    31: "Sticks",
+    32: "Square Click",
+    33: "Metronome Click",
+    34: "Metronome Bell",
+    35: "Acoustic Bass Drum",
+    36: "Bass Drum",
+    37: "Side Stick",
+    38: "Acoustic Snare",
+    39: "Hand Clap",
+    40: "Electric Snare",
+    41: "Low Floor Tom",
+    42: "Closed Hi Hat",
+    43: "High Floor Tom",
+    44: "Pedal Hi-Hat",
+    45: "Low Tom",
+    46: "Open Hi-Hat",
+    47: "Low-Mid Tom",
+    48: "Hi-Mid Tom",
+    49: "Crash Cymbal 1",
+    50: "High Tom",
+    51: "Ride Cymbal 1",
+    52: "Chinese Cymbal",
+    53: "Ride Bell",
+    54: "Tambourine",
+    55: "Splash Cymbal",
+    56: "Cowbell",
+    57: "Crash Cymbal 2",
+    58: "Vibraslap",
+    59: "Ride Cymbal 2",
+    60: "Hi Bongo",
+    61: "Low Bongo",
+    62: "Mute Hi Conga",
+    63: "Open Hi Conga",
+    64: "Low Conga",
+    65: "High Timbale",
+    66: "Low Timbale",
+    67: "High Agogo",
+    68: "Low Agogo",
+    69: "Cabasa",
+    70: "Maracas",
+    71: "Short Whistle",
+    72: "Long Whistle",
+    73: "Short Guiro",
+    74: "Long Guiro",
+    75: "Claves",
+    76: "Hi Wood Block",
+    77: "Low Wood Block",
+    78: "Mute Cuica",
+    79: "Open Cuica",
+    80: "Mute Triangle",
+    81: "Open Triangle",
+    82: "Shaker",
+    83: "Jingle Bell",
+    84: "Bell Tree",
+    85: "Castanets",
+    86: "Mute Surdo",
+    87: "Open Surdo"
+  };
+
   MG.chord_class = {
     "maj": [0, 4, 7],
     "min": [0, 3, 7],
@@ -1828,9 +1892,26 @@ if (typeof module !== 'undefined' && require.main === module) {
     }
 
     AppMG.prototype.reset = function(options) {
-      var playbtns;
+      var ele, k, len, playbtns, ref, version;
       if (options != null) {
-        this.schema = options.schema, this.settings = options.settings, this.contents = options.contents;
+        version = options.version;
+        if (version == null) {
+          version = "0.1";
+        }
+        switch (version) {
+          case "0.1":
+            this.schema = options.schema, this.settings = options.settings, this.contents = options.contents;
+            break;
+          case "0.2":
+            this.schema = options.schema, this.settings = options.settings;
+            this.contents = {};
+            ref = options.contents;
+            for (k = 0, len = ref.length; k < len; k++) {
+              ele = ref[k];
+              this.contents[ele.mode] = ele.data;
+            }
+            this.contents.harmony = options.harmony;
+        }
         this.obj = options;
       } else {
         this.schema = MG.circularClone(MG.schema_summer);
@@ -1853,9 +1934,19 @@ if (typeof module !== 'undefined' && require.main === module) {
 
     AppMG.prototype["export"] = function() {
       return {
+        version: "0.2",
         settings: this.settings,
         schema: this.schema,
-        contents: this.contents
+        harmony: this.contents.harmony,
+        contents: [
+          {
+            "mode": "melody",
+            "data": this.contents.melody
+          }, {
+            "mode": "texture",
+            "data": this.contents.texture
+          }
+        ]
       };
     };
 
@@ -3657,7 +3748,7 @@ function getOutputScale(ctx) {
         });
         return ret;
     })();
-    var keydown = function(evt){
+    function keydown(evt){
         switch(evt.key){
             default:
                 var pitch = keymap[evt.key];
@@ -3868,3 +3959,38 @@ function getOutputScale(ctx) {
 
 })(this, 130, 70, 6, 9);
 
+(function(self){
+    var pressed = {};
+    function pressing(kit){
+        if(typeof pressed[kit] != 'undefined'){
+            return;
+        }
+        var amplitude = parseInt($('#drum_amplitude').val());
+        MIDI.noteOn(9, kit, amplitude);
+        pressed[kit] = kit;
+    }
+    function release(kit){
+        if(typeof pressed[kit] == 'undefined') return;
+
+        delete pressed[kit];
+    }
+    var keymap = {
+        'v': 36, 'f': 37, 'g': 38,
+        'h': 50, 'b': 45, 'n': 41,
+        'y': 49, 'i': 51, 'u': 46, 'j': 44, 'm': 42,
+    };
+    function keydown(evt){
+        if(typeof keymap[evt.key] != 'undefined'){
+            pressing(keymap[evt.key]);
+        }else{
+            // TODO: add other control change
+            // switch
+        }
+    }
+    function keyup(evt){
+        if(typeof keymap[evt.key] != 'undefined'){
+            release(keymap[evt.key]);
+        }
+    }
+    self.drumHandlers = [keydown, keyup];
+})(this)
